47fae60780b3072a6d18da3ca844cd62795216a4
mahyar121 <mahyarhajibabaie@gmail.com>
2016-12-24 17:17:55 -0800

    Programmed the Music to the game

---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    static MusicPlayer instance = null;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        // otherwise make an instance from the class and don't destroy the gameObject which is MusicPlayer
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+ 
+}
