21691ce185de74480c9a4c8dc64d6ecbcc7ec500
mahyar121 <mahyarhajibabaie@gmail.com>
2016-12-24 21:31:22 -0800

    Added mouse control

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        // keep original y position with this.transform.position.y
+        Vector3 paddlePos = new Vector3(4.1f, this.transform.position.y, 0f);
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.4f, 7.7f);
+        this.transform.position = paddlePos;
+        
+	}
+}
